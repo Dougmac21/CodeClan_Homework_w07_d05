70835f6b80d9e4ef7d0efa681d00549d84da6dc7
Douglas MacIntyre <dougmac21@talk21.com>
2020-10-24 19:12:35 +0100

    Adding episodes pt2

--- src/components/AllEpisodesListItem.vue
+++ src/components/AllEpisodesListItem.vue
@@ -1,13 +1,33 @@
 <template>
-  
+  <div>
+        <ul>
+            <li v-on:click='handleClick'>
+            <h2>{{ episode.title }}</h2>
+            <p>{{ episode.synopsis }}</p>
+            </li>
+        </ul>
+  </div>
 </template>
 
 <script>
+import {eventBus} from '@/main.js';
+
 export default {
+    name: 'all-episodes-list-item',
+    props: ['episode'],
+    methods: {
+        handleClick() {
+            eventBus.$emit('episode-selected', this.episode)
+        }
+    }
 
 }
 </script>
 
-<style>
+<style land="css" scoped>
+ul {
+    list-style: none;
+    cursor: pointer;
+}
 
 </style>
--- src/components/EpisodeDetails.vue
+++ src/components/EpisodeDetails.vue
@@ -1,13 +1,19 @@
-<template>
-  
+<template lang="html">
+    <div v-if='episode'>
+        <h2>Episode Detail:</h2>
+        <h3>{{ episode.title }}</h3>
+        <p>{{ episode.plot }}</p>
+    </div>
 </template>
 
 <script>
 export default {
-
+    name: 'episode-details',
+    props: ['episode']
 }
 </script>
 
-<style>
+<style lang="css" scoped>
+
 
 </style>
