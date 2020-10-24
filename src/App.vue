179730184d5a572e8918515cbca22dde7687fb7e
Douglas MacIntyre <dougmac21@talk21.com>
2020-10-24 12:25:06 +0100

    more boilerplating for project scope

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,10 @@
 <script>
 import AllSeriesList from './components/AllSeriesList';
 import SeriesDetails from './components/SeriesDetails';
+import AllSeasonsList from './components'
+import SeasonDetails from './components'
+import AllEpisodesList from './components'
+import EpisodeDetails from './components'
 
 import { eventBus } from '@/main.js'
 
@@ -23,7 +27,9 @@ export default {
       allSeries: [],
       selectedSeries: null,
       allSeasons: [],
-      selectedSeason: null
+      selectedSeason: null,
+      allEpisodes: [],
+      selectedEpisode: null
     }
   },
   methods: {
@@ -32,11 +38,20 @@ export default {
   components: {
     "all-series-list": AllSeriesList,
     "series-details": SeriesDetails,
+    "episode-details": EpisodeDetails
   },
   mounted() {
-    fetch("http://stapi.co/api/v1/rest/season/search")
+    fetch("http://stapi.co/api/v1/rest/series/search")
     .then(res => res.json())
-    .then(all_seasons => this.allSeasons = all_seasons)
+    .then(all_series => this.allSeries = all_series)
+
+    // fetch("http://stapi.co/api/v1/rest/season/search")
+    // .then(res => res.json())
+    // .then(all_seasons => this.allSeasons = all_seasons)
+
+    // fetch("http://stapi.co/api/v1/rest/episode/search")
+    // .then(res => res.json())
+    // .then(all_episodes => this.allEpisodes = all_episodes)
 
   }
 }
