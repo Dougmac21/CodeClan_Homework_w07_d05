a7986d02a724b83d79a4ba2007a8dc85e62c9aef
Douglas MacIntyre <dougmac21@talk21.com>
2020-10-24 13:57:04 +0100

    season detail displays

--- src/components/SeasonDetails.vue
+++ src/components/SeasonDetails.vue
@@ -1,13 +1,21 @@
 <template lang="html">
+    <div v-if='season'>
+        <h2>Season Details:</h2>
+        <h3>{{ season.title }}</h3>
+        <p>{{ season.numberOfEpisodes }}</p>
+
+    </div>
   
 </template>
 
 <script>
 export default {
+    name: 'season-details',
+    props: ['season']
 
 }
 </script>
-
-<style>
+     
+<style lang="css" scoped>
 
 </style>
